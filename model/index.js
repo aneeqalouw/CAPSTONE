017eb837ccff16c0344c917caab21bb1a6f4a0ae
aneeqalouw <aneeqalouw057@gmail.com>
2024-03-05 16:25:49 +0200

    controller user

---
+++ model/index.js
@@ -0,0 +1,13 @@
+import { Users } from "./users";
+import { Courses } from "./courses";
+import { Students } from "./students";
+
+let users = new Users()
+let courses = new Courses()
+let students = new Students()
+
+export{
+    users,
+    courses,
+    students
+}
